1fec79c832dbddedc86877dec00704471f1b32da
Kevin Lamping <klamping@gmail.com>
2018-06-21 11:50:44 -0500

    fix lint issue

--- src/renderer/components/XTerm.vue
+++ src/renderer/components/XTerm.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-  import { Terminal } from 'xterm';
+  // import { Terminal } from 'xterm';
 
   export default {
     name: 'xterm',
@@ -14,25 +14,6 @@
       }
     },
     created () {
-      // eslint-disable-next-line new-parens, new-cap
-      const au = new AnsiUp();
-
-      this.$electron.ipcRenderer.on('test-log', (e, log) => {
-        const html = au.ansi_to_html(log);
-        this.log += `${html}<br />`;
-        // cheap hack to scroll to bottom of container
-        const container = document.getElementById('info-log');
-        container.scrollTop = container.scrollHeight + 200;
-      });
-
-      this.$Message.config({
-        top: 80,
-        duration: 2
-      });
-
-      this.$electron.ipcRenderer.on('test-status', (e, status, type) => {
-        this.$Message[type](status);
-      });
     }
   }
 </script>
