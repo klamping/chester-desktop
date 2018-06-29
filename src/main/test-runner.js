8b82bb46dad94453349c59ac7c2142c14e707a2b
Kevin Lamping <klamping@gmail.com>
2018-06-29 14:16:53 -0500

    var update

--- src/main/test-runner.js
+++ src/main/test-runner.js
@@ -4,24 +4,27 @@ const fixPath = require('fix-path');
 
 fixPath();
 
+let currentRun;
+
 ipc.on('run-test', function (event, folderPath, command) {
   event.sender.send('test-status', 'Test Started', 'info')
 
-  const run = spawn(command, {
+  currentRun = spawn(command, {
     cwd: folderPath,
     stdio: ['ignore', 'pipe', 'pipe'],
     shell: true
   });
 
-  run.stdout.on('data', (data) => {
+  currentRun.stdout.on('data', (data) => {
     event.sender.send('test-log', data.toString('utf8'));
   });
 
-  run.stderr.on('data', (data) => {
+  currentRun.stderr.on('data', (data) => {
     event.sender.send('test-log', data.toString('utf8'));
   });
 
-  run.on('close', (code) => {
+  currentRun.on('close', (code) => {
+    currentRun = null;
     if (code !== 0) {
       // save broken screenshot?
       event.sender.send('test-status', 'Test Run Failure!', 'error');
@@ -30,3 +33,7 @@ ipc.on('run-test', function (event, folderPath, command) {
     }
   });
 })
+
+// ipc.on('debug-command', function (command) {
+
+// })
