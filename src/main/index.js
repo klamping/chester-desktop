f2b5b7ac9062ef9d136e8dd3f94087cc288b9b4f
Kevin Lamping <klamping@gmail.com>
2018-08-24 10:20:54 -0500

    update autoupdater-version

--- src/main/index.js
+++ src/main/index.js
@@ -58,10 +58,38 @@ app.on('activate', () => {
  * support auto updating. Code Signing with a valid certificate is required.
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
-autoUpdater.logger = require('electron-log')
-autoUpdater.logger.transports.file.level = 'info'
+// const log = require('electron-log');
+
+// autoUpdater.logger = log;
+// autoUpdater.logger.transports.file.level = 'info';
+// log.info('App starting...');
 
 app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdatesAndNotify()
-  autoUpdater.checkForUpdatesAndNotify()
 })
+
+// function sendStatusToWindow (text) {
+//   log.info(text);
+//   // win.webContents.send('message', text);
+// }
+// autoUpdater.on('checking-for-update', () => {
+//   sendStatusToWindow('Checking for update...');
+// })
+// autoUpdater.on('update-available', (info) => {
+//   sendStatusToWindow('Update available.');
+// })
+// autoUpdater.on('update-not-available', (info) => {
+//   sendStatusToWindow('Update not available.');
+// })
+// autoUpdater.on('error', (err) => {
+//   sendStatusToWindow('Error in auto-updater. ' + err);
+// })
+// autoUpdater.on('download-progress', (progressObj) => {
+//   let logMessage = 'Download speed: ' + progressObj.bytesPerSecond;
+//   logMessage = logMessage + ' - Downloaded ' + progressObj.percent + '%';
+//   logMessage = logMessage + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
+//   sendStatusToWindow(logMessage);
+// })
+// autoUpdater.on('update-downloaded', (info) => {
+//   sendStatusToWindow('Update downloaded');
+// });
