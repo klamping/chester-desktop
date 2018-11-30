50497d948b62b3638256a20a78e0c90a38111b53
Kevin Lamping <klamping@gmail.com>
2018-11-30 15:59:50 -0600

    fixes #4; makes text a little bigger on project overview screen

--- src/renderer/components/NoProject.vue
+++ src/renderer/components/NoProject.vue
@@ -2,7 +2,7 @@
   <div class="no-project-container">
     <div class="no-project-content">
       <div class="no-projects" v-if="projects.length === 0">
-        <h2>You have no projects setup.</h2>
+        <h2 class="section-title">You have no projects setup.</h2>
         <AddProjectForm></AddProjectForm>
       </div>
       <div class="project-list" v-if="projects.length > 0">
@@ -14,9 +14,9 @@
               {{project.name}}
             </router-link>
           </li>
+          <AddProjectForm></AddProjectForm>
         </ul>
       </div>
-      <AddProjectForm></AddProjectForm>
     </div>
   </div>
 </template>
@@ -50,16 +50,23 @@
     border: 1px solid #ddd;
     border-radius: 5px;
     background: #fff;
-    padding: 30px;
+    padding: 20px 0 0;
     width: 30em;
   }
+  .no-projects .section-title {
+    padding: 0 1em 10px;
+  }
+  .no-projects .add-project {
+    font-size: 1.5em;
+  }
   .project-list h2 {
-    text-align: center;
-    margin-bottom: 1em;
+    text-align: left;
+    margin-left: 20px;
+    margin-bottom: 5px;
   }
   .project-list ul {
     list-style: none;
-    font-size: 1.2em;
+    font-size: 1.3em;
   }
   .project-list li {
     margin: 0;
@@ -67,9 +74,20 @@
   }
   .project-list li a {
     display: block;
-    padding: 10px;
+    padding: 15px 20px 15px 35px;
+  }
+  .project-list li a:hover,
+  .project-list li a:focus {
+    background: #f3f3f3;
   }
   .project-list li:first-child {
     border-top: 0;
+  }
+  .project-list li.add-project-container {
+    border-top: 0;
+    margin-top: 0;
+  }
+  .project-list .add-project {
+    font-size: 1.3em;
   }
 </style>
